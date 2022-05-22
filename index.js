30e5d9ecf854e43995dd7c101ae5e38d581671fd
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-05-22 22:25:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function linkify(inputText) {
 
     //URLs starting with http://, https://, or ftp://
     replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">Link</a>');
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">Click Here</a>');
 
     //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
     replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">Link</a>');
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">Click Here</a>');
 
     // Replace URL starting with t.me
     replacePattern3 = /t.me\/RoyalKrrishna\/[0-9]/;
